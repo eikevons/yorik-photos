36e86c01361bdbdc1cdfdea3c9334e618556de53
Eike von Seggern <eikevons@yahoo.de>
2014-06-21 03:50:10 -0400

    make secret key random

--- photos/config.py
+++ photos/config.py
@@ -1,7 +1,7 @@
 DATABASE = '/var/local/yorik-photos/data/db.sqlite'
 DEBUG = True
 CSRF = True
-SECRET_KEY = 'this is not a secret'
+SECRET_KEY = 'd49f8220e94b4cfea4610a115c9c6b62'
 SERVER_NAME = '5.231.41.206'
 
 PHOTO_STORAGE = '/var/local/yorik-photos/data/photos'
